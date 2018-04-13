44ff9e628d221cddad898995f0a4c3d0740840a5
heistt <supertaoqi@126.com>
2018-04-13 15:06:24 +0800

    add test_mysql.py

---
+++ www/test_mysql.py
@@ -0,0 +1,13 @@
+﻿import orm
+import asyncio
+from models import User, Blog, Comment
+
+loop = asyncio.get_event_loop()
+async def test():
+    await orm.create_pool(loop=loop,user='test', password='test', db='awesome')
+
+    u = User(name='Test', email='test@example.com', passwd='1234567890', image='about:blank')
+
+    await u.save()
+
+loop.run_until_complete(test())
